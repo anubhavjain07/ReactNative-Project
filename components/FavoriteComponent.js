3bb5b83f1dd1c366a99701bad066782992c51997
ANUBHAV JAIN <anubhavjain21799@gmail.com>
2020-05-11 01:50:30 +0530

    Delete Favorites

--- components/FavoriteComponent.js
+++ components/FavoriteComponent.js
@@ -4,6 +4,8 @@ import { ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 import { Loading } from './LoadingComponent';
+import { deleteFavorite } from '../redux/ActionCreators';
+import SwipeOut from 'react-native-swipeout';
 
 const mapStateToProps = (state) => {
     return {
@@ -12,23 +14,38 @@ const mapStateToProps = (state) => {
     }
 }
 
+const mapDispatchToProps = (dispatch) => ({
+    deleteFavorite: (dishId) => dispatch(deleteFavorite(dishId))
+});
 class Favorites extends Component {
 
     render() {
         const { navigate } = this.props.navigation;
+
         const renderMenuItem = ({ item, index }) => {
+            const rightButton = [
+                {
+                    text: 'Delete',
+                    type: 'delete',
+                    onPress: () => this.props.deleteFavorite(item.id)
+                }
+            ];
+            
             return (
-                <ListItem
-                    key={index}
-                    title={item.name}
-                    subtitle={item.description}
-                    hideChevron={true}
-                    onPress={() => navigate('Dishdetail', { dishId: item.id })}
-                    leftAvatar={{ source: { uri: baseUrl + item.image } }}
-                />
+                <SwipeOut right={rightButton} autoClose={true}>
+                    <ListItem
+                        key={index}
+                        title={item.name}
+                        subtitle={item.description}
+                        hideChevron={true}
+                        onPress={() => navigate('Dishdetail', { dishId: item.id })}
+                        leftAvatar={{ source: { uri: baseUrl + item.image } }}
+                    />
+                </SwipeOut>
             );
 
         }
+
         if (this.props.dishes.isLoading) {
             return (<Loading />);
         }
@@ -53,5 +70,5 @@ class Favorites extends Component {
 
 }
 
-export default connect(mapStateToProps)(Favorites);
+export default connect(mapStateToProps, mapDispatchToProps)(Favorites);
 
--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -200,3 +200,8 @@ export const addFavorite = (dishId) => ({
     type: ActionTypes.ADD_FAVORITE,
     payload: dishId
 });
+
+export const deleteFavorite = (dishId) => ({
+    type: ActionTypes.DELETE_FAVORITE,
+    payload: dishId
+});
